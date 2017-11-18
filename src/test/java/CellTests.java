5c81321a6c931cc085efb3cc134d8796b2cfa08a
Berkan <yavrib@gmail.com>
2017-11-19 00:22:56 +0300

    Add test for rule "Cell lives on to the next generation if it has two neighbours".

---
+++ src/test/java/CellTests.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CellTests {
+    @Test
+    public void cell_whenHasTwoNeighbours_shouldBeAlive() {
+        Cell cell = new Cell(CellState.ALIVE);
+
+        cell.neighbours.put(CellDirection.N, new Cell(CellState.ALIVE));
+        cell.neighbours.put(CellDirection.S, new Cell(CellState.ALIVE));
+
+        Assert.assertTrue(CellState.ALIVE.equals(cell.nextState()));
+    }
+}
